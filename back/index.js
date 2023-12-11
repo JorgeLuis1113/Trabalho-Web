99bfa2d9d5c0713568cb21aab79e6f09d870b230
Dangerzin <d2022015148@unifei.edu.br>
2023-12-11 20:34:35 -0300

    Importação dos módulos, classes e criação do servidor

---
+++ back/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require('express');
+const fs = require('fs');
+const cors = require("cors");
+const path = require("path");
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+
+// Criando instância de express
+const app = express();
+
+// Extrair dados de forms com requisição post
+app.use(express.json());
+app.use(cors());
+
+app.listen(3000, () => {
+    console.log('Servidor na porta 3000');
+});
+
+// Importando modelos
+const User = require('./model/User');
+const Inscricao = require('./model/Inscricao');
